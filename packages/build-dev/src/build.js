958c7123311dea19c37a84190739c12a846b041d
oliver <oliver@pengfei.local>
2018-03-07 22:15:50 +0800

    lint test

--- packages/build-dev/src/build.js
+++ packages/build-dev/src/build.js
@@ -1,9 +1,6 @@
 import Service from './Service';
 
 export default function build(opts = {}) {
-  const o = {}
   const service = new Service(opts.cwd, opts);
-  // assaa eslint test
-  return service.build()
-
+  return service.build();
 }
